7fcfed5b5b367c2d4486b8197eb1739f5646d302
Nimrah Gill <Nimrah.Gill@ibm.com>
2019-06-25 16:30:58 -0400

    updated travisTest.sh

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -14,14 +14,8 @@ mvn -q package
 docker build -t system system/.
 docker build -t inventory inventory/.
 
-docker tag system:latest nimg/system
-docker tag inventory:latest nimg/inventory
-
-docker push nimg/system
-docker push nimg/inventory
-
 printf "\replacing containers in kubernetes.yaml\n"
-sed -i 's/guideregistry.azurecr.io/nimg/g' kubernetes.yaml
+sed -i 's/guideregistry.azurecr.io//g' kubernetes.yaml
 
 printf "\nkubectl apply -f kubernetes.yaml\n"
 kubectl apply -f kubernetes.yaml
@@ -38,28 +32,11 @@ kubectl get pods
 printf "\nminikube ip\n"
 echo `minikube ip`
 
-systemStatus="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://`minikube ip`:9080/system/properties/")"
-inventoryStatus="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://`minikube ip`:9081/inventory/systems/")"
-
-if [ "$systemStatus" == "200" ] && [ "$inventoryStatus" == "200" ]
-then
-  echo ENDPOINT OK
-else
-  echo inventory status:
-  echo "$inventoryStatus"
-  echo system status:
-  echo "$systemStatus"
-  echo ENDPOINT
-  exit 1
-fi
-
 printf "\ncurl http://`minikube ip`:9080/system/properties\n"
 curl http://`minikube ip`:9080/system/properties
 
 printf "\ncurl http://`minikube ip`:9081/inventory/systems/system-service\n"
 curl http://`minikube ip`:9081/inventory/systems/system-service
-
-
 
 
 mvn verify -Ddockerfile.skip=true -Dsystem.ip=`minikube ip` -Dinventory.ip=`minikube ip`
