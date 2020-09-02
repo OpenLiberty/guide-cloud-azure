2dea4a8d4410c202a6c631685064bc456d2611f2
Gilbert Kwan <gkwan@ca.ibm.com>
2020-09-02 15:45:43 -0400

    Update travisTest.sh

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -32,3 +32,34 @@ mvn liberty:stop
 
 cd ../system
 mvn liberty:stop
+
+##############################################################################
+##
+##  Test docker
+##
+##############################################################################
+
+docker pull openliberty/open-liberty:kernel-java8-openj9-ubi
+
+docker build -t system:1.0-SNAPSHOT system/.
+docker build -t inventory:1.0-SNAPSHOT inventory/.
+
+sed -i 's/\[inventory-repository-uri\]/inventory/g' kubernetes.yaml
+sed -i 's/\[system-repository-uri\]/system/g' kubernetes.yaml
+
+kubectl apply -f kubernetes.yaml
+
+sleep 120
+
+kubectl get pods
+
+echo `minikube ip`
+
+curl http://`minikube ip`:31000/system/properties
+curl http://`minikube ip`:32000/api/inventory/systems/system-service
+
+mvn failsafe:integration-test -Dsystem.ip=`minikube ip` -Dinventory.ip=`minikube ip`
+mvn failsafe:verify
+
+kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)
+kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)
