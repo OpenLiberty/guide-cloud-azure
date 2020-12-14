ad76bf94d6715c5c45b2accdaffc4261bf300141
Austin Seto <austin.seto@ibm.com>
2020-12-14 15:08:09 -0500

    Add stuff for testing daily docker image

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -9,10 +9,25 @@ do
     esac
 done
 
+echo "Testing daily OpenLiberty image"
+
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" system/pom.xml inventory/pom.xml
 cat system/pom.xml inventory/pom.xml
 
 sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" system/Dockerfile inventory/Dockerfile
 cat system/Dockerfile inventory/Dockerfile
 
-../scripts/testApp.sh
+sudo ../scripts/startMinikube.sh
+sudo ../scripts/testApp.sh
+sudo ../scripts/stopMinikube.sh
+
+echo "Test latest OpenLiberty Docker image"
+
+sed -i "s;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";FROM openliberty/daily:latest;g" system/Dockerfile inventory/Dockerfile
+cat system/Dockerfile inventory/Dockerfile
+
+docker pull "openliberty/daily:latest"
+
+sudo ../scripts/startMinikube.sh
+sudo ../scripts/testApp.sh
+sudo ../scripts/stopMinikube.sh
--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Set up Minikube
-
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x kubectl
-sudo ln -s $(pwd)/kubectl /usr/local/bin/kubectl
-wget https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64 -q -O minikube
-chmod +x minikube
-
-sudo apt-get update -y
-sudo apt-get install -y conntrack
-
-sudo minikube start --vm-driver=none --bootstrapper=kubeadm
-
 # Test app
 
 mvn -q clean package
@@ -41,3 +28,6 @@ mvn liberty:stop
 
 cd ../system
 mvn liberty:stop
+
+# Clear .m2 cache
+rm -rf ~/.m2
