269c6911d3b37ea62b98c416f825ba0c7aef67b6
Austin Seto <austin.seto@ibm.com>
2020-12-16 15:35:16 -0500

    Update for testing docker image

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-while getopts t:d:b:u: flag;
+while getopts t:d: flag;
 do
     case "${flag}" in
         t) DATE="${OPTARG}";;
         d) DRIVER="${OPTARG}";;
-        b) BUILD="${OPTARG}";;
-        u) DOCKER_USERNAME="${OPTARG}";;
     esac
 done
 
-echo "Testing daily OpenLiberty image"
+echo "Test latest OpenLiberty Docker image"
 
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" system/pom.xml inventory/pom.xml
 cat system/pom.xml inventory/pom.xml
@@ -17,7 +15,7 @@ cat system/pom.xml inventory/pom.xml
 sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" system/Dockerfile inventory/Dockerfile
 cat system/Dockerfile inventory/Dockerfile
 
-docker pull $DOCKER_USERNAME"/olguides:"$BUILD
+docker pull "openliberty/daily:latest"
 
 sudo ../scripts/startMinikube.sh
 sudo ../scripts/testApp.sh
