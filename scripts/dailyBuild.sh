3b965f463480da6de1cf07ceea2ca09dbd09bacc
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-31 10:40:39 -0400

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -18,7 +18,7 @@ cat system/pom.xml inventory/pom.xml
 sed -i "s;FROM openliberty/open-liberty:full-java8-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile inventory/Dockerfile
 cat system/Dockerfile inventory/Dockerfile
 
-docker pull $DOCKER_USERNAME"/olguides:"$BUILD
+docker pull "$DOCKER_USERNAME/olguides:$BUILD"
 
 sudo ../scripts/startMinikube.sh
 sudo ../scripts/testApp.sh
