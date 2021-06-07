28196c7ebb66f928dab1a808004ebacec30461eb
Gilbert Kwan <gkwan@ca.ibm.com>
2021-06-07 10:58:04 -0400

    Version update (#48)
    * update versions
    
    * Update dailyBuild.sh
    
    * Update dockerImageTest.sh
    
    * Update dockerImageTest.sh
    
    * Update README.adoc

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -6,18 +6,19 @@ do
         d) DRIVER="${OPTARG}";;
         b) BUILD="${OPTARG}";;
         u) DOCKER_USERNAME="${OPTARG}";;
+        *) echo "Invalid option";;
     esac
 done
 
 echo "Testing daily OpenLiberty image"
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" system/pom.xml inventory/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" system/pom.xml inventory/pom.xml
 cat system/pom.xml inventory/pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" system/Dockerfile inventory/Dockerfile
+sed -i "s;FROM openliberty/open-liberty:full-java8-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile inventory/Dockerfile
 cat system/Dockerfile inventory/Dockerfile
 
-docker pull $DOCKER_USERNAME"/olguides:"$BUILD
+docker pull "$DOCKER_USERNAME/olguides:$BUILD"
 
 sudo ../scripts/startMinikube.sh
 sudo ../scripts/testApp.sh
