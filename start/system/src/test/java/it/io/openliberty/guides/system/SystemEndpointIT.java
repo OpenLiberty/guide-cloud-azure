84286da4037d48ffbb778a280fcbdb402d84cb59
Gilbert Kwan <gkwan@ca.ibm.com>
2022-06-09 15:56:01 -0400

    update versions

--- start/system/src/test/java/it/io/openliberty/guides/system/SystemEndpointIT.java
+++ start/system/src/test/java/it/io/openliberty/guides/system/SystemEndpointIT.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2018, 2020 IBM Corporation and others.
+ * Copyright (c) 2018, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,14 +14,13 @@ package it.io.openliberty.guides.system;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLSession;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
+import jakarta.net.ssl.HostnameVerifier;
+import jakarta.net.ssl.SSLSession;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
 
-import org.apache.cxf.jaxrs.provider.jsrjsonp.JsrJsonpProvider;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -41,7 +40,7 @@ public class SystemEndpointIT {
         systemServiceIp = System.getProperty("system.ip");
         url = "http://" + systemServiceIp + ":" + port + "/system/properties/";
     }
-    
+
     @BeforeEach
     public void setup() {
         response = null;
@@ -62,7 +61,6 @@ public class SystemEndpointIT {
     @Test
     public void testGetProperties() {
         Client client = ClientBuilder.newClient();
-        client.register(JsrJsonpProvider.class);
 
         WebTarget target = client.target(url);
         Response response = target.request().get();
@@ -70,5 +68,5 @@ public class SystemEndpointIT {
         assertEquals(200, response.getStatus(), "Incorrect response code from " + url);
         response.close();
     }
-    
+
 }
