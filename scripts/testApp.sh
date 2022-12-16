32deb2b6182fd8b24d907daded1bd1e28b96a565
Jakub Pomykala <jakub.pomykala@hotmail.co.uk>
2022-12-16 17:52:34 +0000

    adding -ntp to maven

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -3,21 +3,21 @@ set -euxo pipefail
 
 # Test app
 
-mvn -q clean package
+mvn -ntp -q clean package
 
 cd inventory
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package liberty:create liberty:install-feature liberty:deploy
-mvn liberty:start
+mvn -ntp liberty:start
 
 cd ../system
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package liberty:create liberty:install-feature liberty:deploy
-mvn liberty:start
+mvn -ntp liberty:start
 
 cd ..
 
@@ -26,14 +26,14 @@ sleep 120
 curl http://localhost:9080/system/properties
 curl http://localhost:9081/inventory/systems/
 
-mvn failsafe:integration-test -Dsystem.ip="localhost" -Dinventory.ip="localhost"
-mvn failsafe:verify
+mvn -ntp failsafe:integration-test -Dsystem.ip="localhost" -Dinventory.ip="localhost"
+mvn -ntp failsafe:verify
 
 cd inventory
-mvn liberty:stop
+mvn -ntp liberty:stop
 
 cd ../system
-mvn liberty:stop
+mvn -ntp liberty:stop
 
 # Clear .m2 cache
 rm -rf ~/.m2
